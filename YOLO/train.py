322bf7357faa86f097fe85ce7564bd7ce2b4351e
Nemelion13 <nemelion1371@gmail.com>
2026-08-04 19:02:38 +0200

    model training results

--- YOLO/train.py
+++ YOLO/train.py
@@ -1,9 +1,6 @@
 from ultralytics import YOLO
-model = YOLO("yolo26n.pt")  # load a pretrained model (recommended for training)
-results = model.train(data="data.yaml",
-                       epochs=100, 
-                       imgsz=640, 
-                      batch=16, 
-                      device=0, 
-                      name="yolo26n_resistor_color_bands_detection"
-                      )  # train the model
+import os
+path = os.path.join(os.getcwd(), "../runs/detect/yolo26n_resistor_color_bands_detection/weights/last.pt")
+model = YOLO(path)  # load a pretrained model (recommended for training)
+#results = model.train(data="data.yaml",epochs=100, imgsz=640, batch=16,  name="yolo26n_resistor_color_bands_detection")  # train the model
+results = model.train(resume=True)
